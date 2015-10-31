c54d688cab141eae87e070ece9f67f51b52ca55a
Torge Matthies <openglfreak@googlemail.com>
2015-10-31 19:17:41 +0100

    Using git now

---
+++ advanced_opcodes.h
@@ -0,0 +1,5 @@
+#ifndef __ADVANCED_OPCODES_H__
+#define __ADVANCED_OPCODES_H__
+
+
+#endif // __ADVANCED_OPCODES_H__
---
+++ opcodes.h
@@ -0,0 +1,5 @@
+#ifndef __OPCODES_H__
+#define __OPCODES_H__
+
+
+#endif // __OPCODES_H__
